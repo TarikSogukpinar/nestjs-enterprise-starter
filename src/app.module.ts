74270bff982d18abee1aec754ac51d243de1e610
TarikSogukpinar <tariksogukpinar@outlook.com>
2024-10-05 12:01:04 +0300

    feat: Swagger module added for API documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configuration } from './config/index';
+import { SwaggerModule } from './modules/core/swagger/swagger.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    SwaggerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,52 @@
+//Custom Modules, Packages, Configs, etc.
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger, VersioningType } from '@nestjs/common';
+
+//pnpm packages
+import helmet from "helmet"
+import * as hpp from 'hpp';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
+import { SwaggerService } from './modules/core/swagger/swagger.service';
+
+const logger = new Logger();
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
+    logger: console,
   });
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const swaggerService = app.get(SwaggerService);
+  swaggerService.setupSwagger(app);
+
+  app.setGlobalPrefix(
+    configService.get<string>('API_GLOBAL_PREFIX', { infer: true }),
+  );
+  app.enableShutdownHooks();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.use(helmet());
+  app.use(hpp());
+  app.use(compression());
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  const PORT = configService.get<number>('PORT');
+
+  await app.listen(PORT);
+
+  logger.log(`Application listening on port ${PORT}`);
 }
 bootstrap();
