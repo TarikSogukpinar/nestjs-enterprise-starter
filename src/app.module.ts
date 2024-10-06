40d65516b810fb2d383569b1f0b3fa4ad9e21d7c
TarikSogukpinar <tariksogukpinar@outlook.com>
2024-10-06 10:02:37 +0300

    feat: created expection filters

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configuration } from './config/index';
 import { SwaggerModule } from './shared/swagger/swagger.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -13,7 +15,19 @@ import { SwaggerModule } from './shared/swagger/swagger.module';
       isGlobal: true,
       load: [configuration],
     }),
+    DatabaseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT')),
+        user: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+      }),
+      inject: [ConfigService],
+    }),
     SwaggerModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
