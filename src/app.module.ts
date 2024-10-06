3ff6ebf5f7da44f7e45535c6a6f815b46a65ecee
TarikSogukpinar <tariksogukpinar@outlook.com>
2024-10-06 11:07:06 +0300

    feat: gracefulshutdown & throttler module refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GracefulShutdownModule } from 'nestjs-graceful-shutdown';
+import { ThrottlerModule, ThrottlerModuleOptions, ThrottlerOptions, ThrottlerOptionsFactory } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -10,19 +11,40 @@ import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    GracefulShutdownModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
     }),
+    GracefulShutdownModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        gracefulShutdownTimeout: configService.get<number>('GRACEFUL_SHUTDOWN_TIMEOUT'),
+        keepNodeProcessAlive: configService.get<boolean>('KEEP_NODE_PROCESS_ALIVE'),
+        cleanup(app, signal) {
+          app.close();
+          signal ? console.log(`Received signal: ${signal}`) : null;
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => [
+        {
+          ttl: parseInt(configService.get<string>('THROTTLE_TTL', '60'), 10),
+          limit: parseInt(configService.get<string>('THROTTLE_LIMIT', '10'), 10),
+        },
+      ],
+      inject: [ConfigService],
+    }),
     DatabaseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        host: configService.get('DB_HOST'),
-        port: parseInt(configService.get('DB_PORT')),
-        user: configService.get('DB_USER'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT'), 10),
+        user: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
       }),
       inject: [ConfigService],
     }),
